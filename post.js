c987e17f685bd6b180fec8cdf4c95195763e111b
Ryan Lester <hacker@linux.com>
2017-05-26 23:41:05 -0400

    handle Node.js Buffers in memzero

--- post.js
+++ post.js
@@ -3,7 +3,14 @@
 		from_hex: from_hex,
 		from_string: from_string,
 		memcmp: memcmp,
-		memzero: memzero,
+		memzero: function (data) {
+			if (data instanceof Uint8Array) {
+				memzero(data);
+			}
+			else if (typeof Buffer !== 'undefined' && data instanceof Buffer) {
+				data.fill(0);
+			}
+		},
 		to_base64: to_base64,
 		to_hex: to_hex,
 		to_string: to_string
